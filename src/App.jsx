c91c6382bd90a826b8f4369ff7cd3b5eb467c107
Suzy-Lee <>
2024-02-13 13:09:08 +0900

    study: 배열 State 업데이트하기 (챌린지 3 of 4) #5
    비변경 함수를 사용하여 변경 수정하기

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,45 @@
-import ShoppingCart from "./updating-arrays/ShoppingCart";
+import { useState } from 'react';
+import AddTodo from './updating-arrays/AddTodo.jsx';
+import TaskList from './updating-arrays/TaskList.jsx';
 
-export default function App() {
+let nextId = 3;
+const initialTodos = [
+  { id: 0, title: 'Buy milk', done: true },
+  { id: 1, title: 'Eat tacos', done: false },
+  { id: 2, title: 'Brew tea', done: false },
+];
+
+export default function TaskApp() {
+  const [todos, setTodos] = useState(initialTodos);
+
+  function handleAddTodo(title) {
+    setTodos([...todos, { id: nextId++, title: title, done: false }]);
+  }
+
+  function handleChangeTodo(nextTodo) {
+    setTodos(
+      todos.map((t) => {
+        if (t.id === nextTodo.id) {
+          return nextTodo;
+        } else {
+          return t;
+        }
+      })
+    );
+  }
+
+  function handleDeleteTodo(todoId) {
+    setTodos(todos.filter((t) => t.id !== todoId));
+  }
+
   return (
     <>
-      <ShoppingCart />
+      <AddTodo onAddTodo={handleAddTodo} />
+      <TaskList
+        todos={todos}
+        onChangeTodo={handleChangeTodo}
+        onDeleteTodo={handleDeleteTodo}
+      />
     </>
   );
 }
