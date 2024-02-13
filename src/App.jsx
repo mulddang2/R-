7b0e44a1c5b52564b2f253bdcf78e00cba61f039
Suzy-Lee <>
2024-02-13 16:14:48 +0900

    study: State를 사용해 Input 다루기 (챌린지 2 of 3) #7
    프로필 편집기

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,49 @@
 import { useState } from 'react';
-// 사진을 클릭하면 바깥에 있는 <div>의 background--active CSS class를 제거하고 <img>에 picture--active class를 추가합니다. 그리고 배경을 다시 클릭하면 원래 CSS class로 돌아와야 합니다.
 
-// 화면상으로는 사진을 클릭하면 보라색 배경은 제거되고 사진의 테두리는 강조 표시됩니다. 사진 외부를 클릭하면 배경이 강조 표시되고 사진의 테두리 강조 표시는 제거됩니다.
-
-export default function Picture() {
-  const [isActive, setIsActive] = useState(false);
-
-  let backgroundClassName = 'background';
-  let pictureClassName = 'picture';
-
-  if (isActive) {
-    pictureClassName += ' picture--active';
-  } else {
-    backgroundClassName += ' background--active';
-  }
+export default function EditProfile() {
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [isEditing, setIsEditing] = useState(false);
 
   return (
-    <div className={backgroundClassName} onClick={() => setIsActive(false)}>
-      <img
-        onClick={(e) => {
-          e.stopPropagation();
-          setIsActive(true);
-        }}
-        className={pictureClassName}
-        alt='Rainbow houses in Kampung Pelangi, Indonesia'
-        src='https://i.imgur.com/5qwVYb1.jpeg'
-      />
-    </div>
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+        setIsEditing(!isEditing);
+      }}
+    >
+      <label>
+        First name:{' '}
+        {isEditing ? (
+          <input
+            value={firstName}
+            onChange={(e) => {
+              setFirstName(e.target.value);
+            }}
+          />
+        ) : (
+          <b>{firstName}</b>
+        )}
+      </label>
+      <label>
+        Last name:{' '}
+        {isEditing ? (
+          <input
+            value={lastName}
+            onChange={(e) => {
+              setLastName(e.target.value);
+            }}
+          />
+        ) : (
+          <b>{lastName}</b>
+        )}
+      </label>
+      <button type='submit'>{isEditing ? 'Save' : 'Edit'} Profile</button>
+      <p>
+        <i>
+          Hello, {firstName} {lastName}!
+        </i>
+      </p>
+    </form>
   );
 }
