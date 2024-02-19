e9a34a31d60ebee3b0c928d3f5c1ad144edfa2c2
Suzy-Lee <>
2024-02-19 15:46:48 +0900

    study: 컴포넌트 간 State 공유하기 (챌린지 1 of 2) #9
    동기화된 입력

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,25 @@
+/* eslint-disable react/prop-types */
+// NOTE: 챌린지 1 of 2: 동기화된 입력 
 import { useState } from 'react';
-import { letters } from './choosing-the-state-structure/data';
-import Letter from './choosing-the-state-structure/Letter';
-// NOTE: 배열의 단점으로 includes 사용 시, selectedIds.includes(letter.id)를 호출해서 선택여부확인을 한다는 것이다. 그래서 State에 Set을 보관하고, Set 활용해서 has 메서드 쓰면 좀더 빠르다
-export default function MailClient() {
-  const [selectedIds, setSelectedIds] = useState([]);
 
-  // TODO: allow multiple selection
-  const selectedCount = selectedIds.length;
+export default function SyncedInputs() {
+  const [text, setText] = useState('');
 
-  function handleToggle(toggledId) {
-    // TODO: allow multiple selection
-
-    if (selectedIds.includes(toggledId)) {
-      setSelectedIds(selectedIds.filter((id) => id !== toggledId));
-    } else {
-      setSelectedIds([...selectedIds, toggledId]);
-    }
+  function handleChange(e) {
+    setText(e.target.value);
   }
-
   return (
     <>
-      <h2>Inbox</h2>
-      <ul>
-        {letters.map((letter) => (
-          <Letter
-            key={letter.id}
-            letter={letter}
-            isSelected={
-              // TODO: allow multiple selection
-              selectedIds.includes(letter.id)
-            }
-            onToggle={handleToggle}
-          />
-        ))}
-        <hr />
-        <p>
-          <b>You selected {selectedCount} letters</b>
-        </p>
-      </ul>
+      <Input label='First input' value={text} onChange={handleChange} />
+      <Input label='Second input' value={text} onChange={handleChange} />
     </>
+  );
+}
+
+function Input({ label, value, onChange }) {
+  return (
+    <label>
+      {label} <input value={value} onChange={onChange} />
+    </label>
   );
 }
